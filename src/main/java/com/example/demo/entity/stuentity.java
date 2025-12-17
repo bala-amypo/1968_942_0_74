a9c2956505fc410f15b8bbce30ff39df2a5d8920
bala-amypo <bala@amypo.in>
2025-12-17 05:19:43 +0000

    User commit

--- src/main/java/com/example/demo/entity/stuentity.java
+++ src/main/java/com/example/demo/entity/stuentity.java
@@ -26,5 +26,26 @@ public class stuentity{
     public void setName(String name){
         this.name=name;
     }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public float getCgpa(){
+        return cgpa;
+    }
+    public void setCgpa(float cgpa){
+        this.cgpa=cgpa;
+    }
+    public stuentity(Long id,String name,String email,float cgpa){
+        this.id=id;
+        this.name=name;
+        this.email=email;
+        this.cgpa=cgpa;
+    }
+    public stuentity(){
+        
+    }
 
 }
