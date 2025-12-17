d6a2410edbf627614223ee76647ab5579e7ea053
bala-amypo <bala@amypo.in>
2025-12-17 05:14:43 +0000

    User commit

--- src/main/java/com/example/demo/entity/stuentity.java
+++ src/main/java/com/example/demo/entity/stuentity.java
@@ -2,8 +2,29 @@ package com.example.demo.entity;
 
 import jakarata.persistence.Entity;
 import jakarata.persistence.GeneratedValue;
-import jakarata.persistence.GeneratedValue
+import jakarata.persistence.GenerationType;
+import jakarata.persistence.Id;
+
 @Entity
 public class stuentity{
+    @Id
+    @GeneratedValue(stragy=GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private float cgpa;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
 
 }
