a5f1a9787afcc325789e3938863ed124135ece06
bala-amypo <bala@amypo.in>
2025-12-17 05:09:43 +0000

    User commit

--- src/main/java/com/example/demo/entity/stuentity.java
+++ src/main/java/com/example/demo/entity/stuentity.java
@@ -1,6 +1,9 @@
 package com.example.demo.entity;
 
-@
+import jakarata.persistence.Entity;
+import jakarata.persistence.GeneratedValue;
+import jakarata.persistence.GeneratedValue
+@Entity
 public class stuentity{
 
 }
